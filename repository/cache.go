096f046d6c4fe1faa734fa284e38d72c56082ec0
aleksanderKirov43 <region-manager43@yandex.ru>
2025-04-15 13:34:49 +0300

    add redis

--- repository/cache.go
+++ repository/cache.go
@@ -1,30 +1,29 @@
 package repository
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"sync"
 	"time"
+
+	"github.com/go-redis/redis/v8"
 )
 
-// Значения, для хранения в кеше
-type CacheEntry struct {
-	value      map[string]string
-	expiration int64
-}
+var ctx = context.Background()
+var rdb *redis.Client
 
-// Кэш
-type SafeCache struct {
-	syncMap sync.Map
+func init() {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
 }
-
-var cache = NewSafeCache()
 
 func GetDataHandler(w http.ResponseWriter, r *http.Request) {
-
-	value, found := cache.Get("data")
-	if !found {
+	value, err := rdb.Get(ctx, "data").Result()
+	if err == redis.Nil {
 		data := map[string]string{
 			"google":     "google.com",
 			"yahoo!":     "search.yahoo.com",
@@ -36,66 +35,21 @@ func GetDataHandler(w http.ResponseWriter, r *http.Request) {
 			"archive":    "archive.org",
 			"ecosia":     "ecosia.org",
 		}
-		cache.Set("data", data, 1*time.Minute)
-		value = data
-
+		jsonData, _ := json.Marshal(data)
+		err := rdb.Set(ctx, "data", jsonData, 10*time.Minute).Err()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		value = string(jsonData)
 		fmt.Println("Кеш валидирован")
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	} else {
 		fmt.Println("Кеш не валидирован")
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	jsonResponse, err := json.Marshal(value)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Write(jsonResponse)
-}
-
-func NewSafeCache() *SafeCache {
-	return &SafeCache{}
-}
-
-// Сохраняем значения с заданными TTL
-// Задаём "врмя жизни" в секундах.
-func (sc *SafeCache) Set(key string, data map[string]string, ttl time.Duration) {
-	expiration := time.Now().Add(ttl).UnixNano()
-	sc.syncMap.Store(key, CacheEntry{value: data, expiration: expiration})
-
-}
-
-// Извлекаем значение из кэша, если оно не найдено
-// или истёк срок действия.
-func (sc *SafeCache) Get(key string) (map[string]string, bool) {
-	entry, found := sc.syncMap.Load(key)
-	if !found {
-		return nil, false
-	}
-
-	cacheEntry := entry.(CacheEntry)
-	if time.Now().UnixNano() > cacheEntry.expiration {
-		sc.syncMap.Delete(key)
-		return nil, false
-	}
-	return cacheEntry.value, true
-}
-
-// Удаление значения из кэша
-func (sc *SafeCache) Delete(key string) {
-	sc.syncMap.Delete(key)
-}
-
-// Переодическая очистка кэша
-func (sc *SafeCache) CleanUp() {
-	for {
-		time.Sleep(1 * time.Minute)
-		sc.syncMap.Range(func(key, entry interface{}) bool {
-			cacheEntry := entry.(CacheEntry)
-			if time.Now().UnixNano() > cacheEntry.expiration {
-				sc.syncMap.Delete(key)
-			}
-			return true
-		})
-	}
+	w.Write([]byte(value))
 }
